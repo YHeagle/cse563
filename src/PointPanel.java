deb52d0f1b1c4244dabb83fe75d77bd61ac632ec
Hanut Singh <70129367+hanut1909@users.noreply.github.com>
2022-04-26 20:52:47 -0700

    Update PointPanel.java

--- src/PointPanel.java
+++ src/PointPanel.java
@@ -1,7 +1,8 @@
+import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Random;
 import java.util.*;
 import javax.swing.JPanel;
+
 public class PointPanel extends JPanel
 {
 	private int randomNum = 50;
@@ -13,11 +14,40 @@ public class PointPanel extends JPanel
 		//this.repaint();
 	}
 	
+	ArrayList<Point> gridPoint  = new ArrayList<>();
+	Dbscan d = new Dbscan(gridPoint,50);
+	
+	ArrayList<ArrayList<Pair>> result= d.runDbscan();
+	
+	public int X1, X2, Y1, Y2;
+	
 	@Override
 	public void paint(Graphics arg0) {
+		
+		for (int i = 0; i < result.size(); i++) {
+	        for (int j = 0; j < result.get(i).size(); j++) {
+	        	X1=result.get(i).get(j).point1.getX();
+	        	Y1=result.get(i).get(j).point1.getY();
+	        	X2=result.get(i).get(j).point2.getX();
+	        	Y2=result.get(i).get(j).point2.getY();
+	        	
+	        	arg0.setColor(Color.red);
+	        	arg0.drawLine(X1, Y1, X2, Y2);
+	        	
+	        	try {
+					arg0.wait(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        	
+	        }
+	    }
+		
 		for(Point p : points)
 		{
 			p.draw(arg0);
+			
 		}
 	}
 	
@@ -29,7 +59,6 @@ public class PointPanel extends JPanel
 		this.points = points;
 	}
 	
-	
 	//clear the point Linkedlist
 	public void clear()
 	{
@@ -40,7 +69,6 @@ public class PointPanel extends JPanel
 	{   
 		
 		Random rand = new Random();
-		  				
 		for(int i = 0; i < randomNum; i++)
 		{   
 			int x = rand.nextInt(800);
@@ -49,8 +77,4 @@ public class PointPanel extends JPanel
 		}
 	}
 	
-	
-	
-	
-
 }
